60b5fb2f99f53c883ca5c654f92b36b286ac6ef6
Christoph Fink <christoph.fink@helsinki.fi>
2017-10-26 13:09:28 +0300

    also update caption (utf8 for emojis)

--- tools/mansicca-images/download-one-instagram-image.sh
+++ tools/mansicca-images/download-one-instagram-image.sh
@@ -7,17 +7,22 @@ table="${3}"
 
 wd="$(realpath "$(dirname "${0}")")"
 
-display_url="$(wget "${url}" -qO - |grep display_url|sed 's/.*"display_url": "\([^"]*\)",.*/\1/')"
+index="$(mktemp)";
 
+wget "${url}" -q -O "${index}"
+
+display_url="$(cat "${index}" |grep display_url|sed 's/.*"display_url": "\([^"]*\)",.*/\1/')"
 if [[ -n "$display_url" ]]; then
 
     photo="$(echo ${url} | sed 's/^.*\/\([^\/]*\)\/$/\1/')"
+    d="$(echo "${photo}" | cut -c1)"
+    mkdir -p "${wd}/images/${d}/"
 
-    if [[ -e "${wd}/images/${photo}.jpg" ]]; then
+    if [[ -e "${wd}/images/${d}/${photo}.jpg" ]]; then
         echo -n "*"
     else
-        wget -cqO "${wd}/images/${photo}.jpg" "${display_url}"
-    echo -n "+"
+        wget -cqO "${wd}/images/${d}/${photo}.jpg" "${display_url}"
+        echo -n "+"
     fi
 
     echo "update ${table} set photo='${photo}.jpg' where url='${url}'" | psql -t "${pgconn}" -q
@@ -26,3 +31,8 @@ else
     echo "update ${table} set photo='[none]' where url='${url}'" | psql -t "${pgconn}" -q
     echo -n "-"
 fi
+
+caption="$(cat "${index}" | grep "window._sharedData" | sed 's/^.*window._sharedData = \({.*}\)[^}]*$/\1/g'|json_reformat |grep -A 6 edge_media_to_caption|grep '"text":'|sed 's/^.*"text": "\(.*\)"$/\1/')"
+echo "update ${table} set caption='${caption}' where url='${url}';" | psql -t "${pgconn}" -q
+
+rm -f "${index}"
